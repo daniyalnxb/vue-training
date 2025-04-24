6ad7054f311c28289e10da8e546d3368d65a6299
daniyal.tayyab <daniyal.tayyab@nxb.com.pk>
2025-04-24 17:21:57 +0500

    fetching destination data from remote server

--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,16 +1,32 @@
 <script setup>
-    import { computed } from 'vue';
+    import { computed, onMounted, ref, watch } from 'vue';
     import { useRoute } from 'vue-router';
 
     import { destinations } from '@/destinations-data';
 
     const route = useRoute();
     const destinationId = computed(() => parseInt(route.params.id));
-    const destination = computed(() => destinations?.find((dest) => dest.id === destinationId.value));
+    // const destination = computed(() => destinations?.find((dest) => dest.id === destinationId.value));
+    // here we no longer need the computed property destination as we are fetching it from server.
+    const destination = ref(null);
+    const baseUrl = 'https://travel-dummy-api.netlify.app';
+
+    const fetchDestinationData = async () => {
+        const response = await fetch(`${baseUrl}/${route.params.slug}`);
+        destination.value = await response.json();
+    }
+
+    onMounted(() => {
+        fetchDestinationData();
+    });
+
+    watch(() => route.params, async () => {
+        fetchDestinationData();
+    });
 </script>
 
 <template>
-    <section class="destination">
+    <section v-if="destination" class="destination">
         <div>
             <h1>{{ destination?.name }}</h1>
             <div class="destination-details">
@@ -19,4 +35,5 @@
             </div>
         </div>
     </section>
+    <h1 v-if="!destination">No data found</h1>
 </template>
